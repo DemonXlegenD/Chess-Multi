cf931a613bddb8a9df171de362faaaafbdcf73b5
f.lahalle <f.lahalle@student.isartdigital.com>
2024-11-05 22:37:09 +0100

    Start button fix

--- Assets/Scripts/Menu/PanelLobby.cs
+++ Assets/Scripts/Menu/PanelLobby.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PanelLobby : MonoBehaviour
 {
-    private bool isHost = false;
     [SerializeField] private BlackBoard blackBoard;
     [SerializeField] private RectTransform startRectTransform;
 
@@ -13,13 +12,13 @@ public class PanelLobby : MonoBehaviour
         startRectTransform.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (blackBoard.GetValue<bool>(DataKey.IS_HOST) && !isHost)
-        {
+    public void DisplayStartButton()
+    { 
             startRectTransform.gameObject.SetActive(true);
-            isHost = true;
-        }
+    }
+
+    public void HideStartButton()
+    {
+        startRectTransform.gameObject.SetActive(false);
     }
 }
